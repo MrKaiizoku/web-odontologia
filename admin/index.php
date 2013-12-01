59fa3a8ce99d556983f710880c3f02c54c44f082
MrKaiizoku <mrkaiizoku@gmail.com>
2013-12-01 13:09:53 -0300

    Primera Versión
    --> Página del lado del cliente
    --> Se debe implementar página administrativa

---
+++ admin/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Jacqueline Arocena - Odontóloga</title>
+    <script type="text/javascript" src="jquery-1.7.1.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="css3, html5 responsive diseno, dise&ntilde;o web" />
+    <meta name="author" content="" />
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="../stylesheets/style.css" />
+    <script type="text/javascript" src="files/modernizr.custom.79639.js"></script>
+	<link rel="shortcut icon" href="favicon.png" type="image/png" />
+		<!--[if lte IE 8]>
+			 <link rel="stylesheet" type="text/css" href="stylesheets/ie8.css" />
+		<![endif]-->
+</head>
+<body>
+<body>
+
+    <div class="container">
+
+        <input type="radio" name="radio-set" checked="checked" id="radio-1"/>
+                <a href="#home">Nuevo Cliente</a>
+        <input type="radio" name="radio-set" id="radio-2"/>
+                <a href="#sobre">Modificar Cliente</a>
+        <input type="radio" name="radio-set"  id="radio-3"/>
+                <a href="#contacto">Consultar Cliente</a>
+
+
+
+        <div class="scroll">
+
+            <section class="panel color-1" id="home">
+                <h2>Nuevo Cliente</h2>
+				<center>
+					<form 
+					<table>
+						<tr><td>Nombre</td><td></td></tr>
+						<tr><td></td><td></td></tr>
+						<tr><td></td><td></td></tr>
+					</table>
+				</center>
+             </section>
+
+            <section class="panel color-2" id="sobre">
+                <h2>Consultar/Modificar Cliente</h2>
+            </section>
+
+            <section class="panel color-3" id="contacto">
+                <h2>Consultas</h2>
+                
+			</section>
+
+        </div> <!-- fin del scroll -->
+
+
+    </div> <!-- fin del container -->
+    <div class="clr"></div>
+    <footer>
+		<a href="../index.html">Página Principal</a>
+    </footer>
+
+</body>
+</html>
